3aada53bae9152ed4c8ef0f496f99585b25eaf52
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-07-09 13:31:29 -0400

    pug and a post route started

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
+
+
 app.set('view engine', 'pug');//tells Express where to look and to use pug
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello There</h1>'); //establish home 
+    response.render('index'); //establish home 
+});
+
+app.get('/cards', (request, response) => {
+    response.render('card', { prompt: "Who dies in 323?" }); //establish a second page 
 });
 
 app.get('/hello', (request, response) => {
-    response.send('<h1>General Kenobi</h1>'); //establish a second page 
+    response.render('hello');  
+});
+
+app.post('/hello', (request, response) => {
+    response.render('hello');  
 });
 
 app.listen(3000, () => {
