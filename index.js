40b0b3f307153f12a02a36c911b7c02a546c66a6
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-07-09 22:41:22 -0400

    more routes made

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { response, request } = require('express');
 
 const app = express();
-
-
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');//tells Express where to look and to use pug
 
 app.get('/', (request, response) => {
-    response.render('index'); //establish home 
+    const name = request.cookies.username
+    if (name) {
+        response.render('index', { name }); //establish home
+    } else {
+        response.redirect('/hello');
+    }  
 });
 
 app.get('/cards', (request, response) => {
-    response.render('card', { prompt: "Who dies in 323?" }); //establish a second page 
+    response.render('card', { prompt: "Who dies in 323?" }); //establish a second page and passes in a prompt 
 });
 
 app.get('/hello', (request, response) => {
-    response.render('hello');  
+    const name = request.cookies.username;
+    if (name) {
+        response.redirect('/')
+    } else {
+        response.render('hello');  
+    }
 });
 
 app.post('/hello', (request, response) => {
-    response.render('hello');  
+    response.cookie('username', request.body.username);
+    response.redirect('/');  
 });
+
+app.post('/goodbye', (request, response) => {
+    response.clearCookie('username');
+    response.redirect('/hello');
+})
 
 app.listen(3000, () => {
     console.log("running!");
